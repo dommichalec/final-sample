906690cdbdd5ad188250c5eac93fd125b3f1617f
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-12-14 12:04:27 -0500

    tighten up full_title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # returns the full title of each page on the page's tab
+  def full_title(title="")
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if title.strip.empty?
+      base_title
+    else
+      title.strip + " | " base_title}
+    end
+  end
 end
