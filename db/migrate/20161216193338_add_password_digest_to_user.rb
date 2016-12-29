d7b5c353b712e8b2eab732a7183d59ac04d9f55e
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-12-29 16:41:12 -0500

    drop db and rest

--- db/migrate/20161216193338_add_password_digest_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordDigestToUser < ActiveRecord::Migration[5.0]
-  # rails g migration AddPasswordDigestToUser password_digest:string
-  def change
-    add_column :users, :password, :digest
-  end
-end
